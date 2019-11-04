51d7c164b2c5d13ff5ed9293023f924742723438
Peter Bregnholm Nielsen <pebn@tv2.dk>
2019-11-04 14:03:11 +0100

    Updates

---
+++ precache-manifest.e327df46cdc91ac6ee01a2b8342dfa52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f05b9e4419bce61aa8b9a43bf23f016c",
+    "url": "/eggs/index.html"
+  },
+  {
+    "revision": "ee8012f1eab7fe93a255",
+    "url": "/eggs/static/css/main.9cd389e0.chunk.css"
+  },
+  {
+    "revision": "aba36bf8f07624c32225",
+    "url": "/eggs/static/js/2.0c6f2633.chunk.js"
+  },
+  {
+    "revision": "ee8012f1eab7fe93a255",
+    "url": "/eggs/static/js/main.e902d999.chunk.js"
+  },
+  {
+    "revision": "c01c23a788e2b0d28a23",
+    "url": "/eggs/static/js/runtime-main.a422143c.js"
+  }
+]);

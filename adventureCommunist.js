2435ccb3a6a2535bd493de8c87cc85685ac69d9d
Peter Nielsen <bregnholm@banbib.dk>
2020-05-27 08:09:42 +0200

    Update adventurecommunist

--- adventureCommunist.js
+++ adventureCommunist.js
@@ -11,6 +11,15 @@ class AdventureCommunist {
         this.making = 0;
         this.having = 0;
         this.want = 0;
+        this.percentage = 0;
+        this.bonus = 0;
+
+    }
+
+    get addPercentagePower() {
+        const power = this.bonus * this.percentage;
+
+        return power || 1;
     }
 
     get iNeed() {
@@ -29,24 +38,43 @@ class AdventureCommunist {
     }
 
     get checkInTime() {
-        return chalk.greenBright(new Date(this.curTime + this.timeItTakes).toTimeString());
+        const date = new Date(this.curTime + this.timeItTakes);
+        return chalk.greenBright(date.toLocaleDateString(), date.toLocaleTimeString());
     }
 
 
-    get timeObject() {
+    get humanReadableTime() {
         const timing = moment.duration(this.timeItTakes);
-        return {
-            years: timing.years(),
-            months: timing.months(),
-            days: chalk.green(timing.days(), timing.days() === 1 ? 'day' : 'days'),
-            hours: chalk.green(timing.hours(), 'hours'),
-            minutes: chalk.green(timing.minutes(), 'minutes'),
-            seconds: timing.seconds(),
-        };
+
+        const ready = [];
+
+        if(timing.years()) {
+            ready.push(chalk.green(timing.years(), timing.years() === 1 ? 'year' : 'years'));
+        }
+
+        if(timing.months()) {
+            ready.push(chalk.green(timing.months(), timing.months() === 1 ? 'month' : 'months'));
+        }
+        
+        if(timing.days()) {
+            ready.push(chalk.green(timing.days(), timing.days() === 1 ? 'day' : 'days'));
+        }
+
+
+        if(timing.hours()) {
+            ready.push(chalk.green(timing.hours(), timing.hours() === 1 ? 'hour' : 'hours'));
+        }
+ 
+        if(timing.minutes()) {
+            ready.push(chalk.green(timing.minutes(), timing.minutes() === 1 ? 'minute' : 'minutes'));
+        }
+        const last = ready.pop();
+
+        return ready.join(", ") + ' and ' + last;
     }
 
     makePerSecond(make = 0, string = '') {
-        this.making = this.turnInto(make, string);
+        this.making = this.turnInto(make * this.addPercentagePower , string);
     }
 
     iHave(have = 0, string = '') {
@@ -66,21 +94,28 @@ class AdventureCommunist {
 }
 
 const adventures = [];
-adventures[0] = new AdventureCommunist('Shuriken');
-adventures[0].makePerSecond(1.19, 'HH');
-adventures[0].iHave(23.51, 'II');
-adventures[0].iWant(25,'II');
+adventures[0] = new AdventureCommunist('comrades for Hospitals');
+//adventures[0].percentage = 0.27;
+//adventures[0].bonus = 32760;
+adventures[0].multiplier = 5;
+adventures[0].makePerSecond(708.609, 'Thousand');
+adventures[0].iHave(11.69, 'B');
+adventures[0].iWant(23,'B');
 
-adventures[1] = new AdventureCommunist('Shoguns');
-adventures[1].makePerSecond(1.20, 'HH');
-adventures[1].iHave(267 * 100, 'HH');
-adventures[1].iWant(700 * 100, 'HH');
+adventures[1] = new AdventureCommunist('Potatoes');
+adventures[1].percentage = 0.27;
+adventures[1].bonus = 32760;
+adventures[1].makePerSecond(4.26, 'LLL');
+adventures[1].iHave(2.25, 'OOO');
+adventures[1].iWant(3.7, 'OOO');
 
 
-adventures[2] = new AdventureCommunist('Kaiju');
-adventures[2].makePerSecond(10.45, 'EE');
-adventures[2].iHave(20 * 10, 'FF');
-adventures[2].iWant(50 * 10, 'FF');
+adventures[2] = new AdventureCommunist('Airports');
+adventures[2].percentage = 0.27;
+adventures[2].bonus = 32760;
+adventures[2].makePerSecond(137.9, 'HHH');
+adventures[2].iHave(70, 'KFC');
+adventures[2].iWant(7378, 'KFC');
 
 
 console.clear();
@@ -88,11 +123,10 @@ console.log(chalk.bgRed(`                                                       
 console.log(chalk.bgRed("                              ADVENTURE COMMUNIST CALCULATIONS                               "));
 console.log(chalk.bgRed(`                                                                                             `));
 adventures.forEach(adventure => {
-    const { days, hours, minutes } = adventure.timeObject;
     console.log(`\n`);
     console.log(`   I need:                 ${adventure.neededStill} ${adventure.title}`);
-    console.log(`   Should have that in:    ${days}, ${hours} and ${minutes}`);
-    console.log(`   So check in at:         ${adventure.checkInTime}`);
+    console.log(`   Should have that in:    ${adventure.humanReadableTime}`);
+    console.log(`   So check in:            ${adventure.checkInTime}`);
 })
 console.log(`\n`);
 console.log(chalk.bgBlackBright(`                                                                                             `));

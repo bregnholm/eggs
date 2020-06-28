9b7bc7fbfc5fcf7cb67b8877af0245c5b7b2f24b
Peter Nielsen <bregnholm@banbib.dk>
2020-06-28 10:37:33 +0200

    Use hash

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link
@@ -16,28 +16,28 @@ import Home from "./Home";
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/">
       <div className="App">
         <Navbar expand="lg" variant="dark">
-          <Navbar.Brand href="/eggs">Peters Stuff</Navbar.Brand>
+          <Navbar.Brand href="#">Peters Stuff</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="/eggs/">Home</Nav.Link>
-                    <Nav.Link href="/eggs/eggs">Eggs</Nav.Link>
-                    <Nav.Link href="/eggs/walks">Walks</Nav.Link>
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#eggs">Eggs</Nav.Link>
+                    <Nav.Link href="#walks">Walks</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
 
         <Switch>
-            <Route path="/eggs/eggs">
+            <Route path="/eggs">
                 <Eggs />
             </Route>
-            <Route path="/eggs/walks">
+            <Route path="/walks">
                 <Walks />
             </Route>
-            <Route path="/eggs/">
+            <Route path="/">
                 <Home />
             </Route>
         </Switch>

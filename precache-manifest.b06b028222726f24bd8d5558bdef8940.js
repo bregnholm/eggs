99ae4d7ca86201bbdb0df56cf64a979295dd21f2
Peter Bregnholm Nielsen <pebn@tv2.dk>
2020-06-28 10:14:59 +0200

    Updates

---
+++ precache-manifest.b06b028222726f24bd8d5558bdef8940.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fee37d10e4567475df502ad7b75ff636",
+    "url": "/eggs/index.html"
+  },
+  {
+    "revision": "e24e1f6e42ef9503ca3f",
+    "url": "/eggs/static/css/2.c548bf2f.chunk.css"
+  },
+  {
+    "revision": "e33653703666e7289f03",
+    "url": "/eggs/static/css/main.803b91a4.chunk.css"
+  },
+  {
+    "revision": "e24e1f6e42ef9503ca3f",
+    "url": "/eggs/static/js/2.3ba95b3f.chunk.js"
+  },
+  {
+    "revision": "e33653703666e7289f03",
+    "url": "/eggs/static/js/main.ad5d5362.chunk.js"
+  },
+  {
+    "revision": "c01c23a788e2b0d28a23",
+    "url": "/eggs/static/js/runtime-main.a422143c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/eggs/precache-manifest.fb541fe9b60e53db71935be8d775941d.js"
+  "/eggs/precache-manifest.b06b028222726f24bd8d5558bdef8940.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eggs/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

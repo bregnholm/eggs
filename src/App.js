228f3d8dc18ff4d6edc193d2768a8416dc311e50
Peter Nielsen <bregnholm@banbib.dk>
2020-08-20 20:30:25 +0200

    Remove unneeded code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import React from "react";
 import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Link
+  BrowserRouter as Router,
 } from "react-router-dom";
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
 import Eggs from "./Eggs";
-import Walks from "./Walks";
 import './App.css';
-import Home from "./Home";
 
 
 export default function App() {
@@ -19,28 +14,17 @@ export default function App() {
     <Router basename="/">
       <div className="App">
         <Navbar expand="lg" variant="dark">
-          <Navbar.Brand href="#">Peters Stuff</Navbar.Brand>
+          <Navbar.Brand href="/">Peters Stuff</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#">Home</Nav.Link>
-                    <Nav.Link href="#eggs">Eggs</Nav.Link>
-                    <Nav.Link href="#walks">Walks</Nav.Link>
+                    <Nav.Link href="/">Home</Nav.Link>
+                    <Nav.Link href="/eggs">Eggs</Nav.Link>
+                    <Nav.Link href="/walks">Walks</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
-
-        <Switch>
-            <Route path="/eggs">
-                <Eggs />
-            </Route>
-            <Route path="/walks">
-                <Walks />
-            </Route>
-            <Route path="/">
-                <Home />
-            </Route>
-        </Switch>
+        <Eggs />
         <footer></footer>
       </div>
     </Router>
